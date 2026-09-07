b06b9c2ba17ddf4f552ce90c1ab046d4e9b391d5
macayu17 <63203492+macayu17@users.noreply.github.com>
2026-09-07 02:21:02 +0000

    chore: update github-linguist SHA

--- internal/generate/knownfiles/main.go
+++ internal/generate/knownfiles/main.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	linguistSHA = "c94cbf94cb25296f71cdedc5051aa18f556b3c9d" // DevSkim: ignore DS173237 - this is a commit SHA, not a secret
+	linguistSHA = "5fb5096b95ab9893c5925d87121e5faaae9f3966" // DevSkim: ignore DS173237 - this is a commit SHA, not a secret
 	linguistURL = "https://raw.githubusercontent.com/github-linguist/linguist/" + linguistSHA + "/lib/linguist/languages.yml"
 	outputFile  = "known_files_gen.go"
 )
